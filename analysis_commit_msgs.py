47fe4c2409742a696029f16608214565b9ce7e31
natalia.murycheva <nataliamurycheva94@gmail.com>
2019-07-04 18:41:05 +0300

    add messages' statistics for user <builder>

--- analysis_commit_msgs.py
+++ analysis_commit_msgs.py
@@ -1,4 +1,5 @@
 import os
+from subprocess import call
 
 
 def collect_statistics_no_msg_commits(com_com_log: str):
@@ -39,10 +40,23 @@ def collect_statistics_msg_frequency(com_com_log: str, output_file: str):
             file.write(f"{c} \t {len(count_vs_message[c])} \t {count_vs_message[c]}\n")
 
 
+def create_com_com_log_file_for_one_author(com_com_log: str, git_dir: str):
+    # logic
+    call(f'echo "parent_commit_file_hash; current_commit_file_hash; message; author; date;" > {com_com_log}', shell=True)
+    cd_command = f"cd {git_dir}"
+    git_log_command = f'git log --pretty="%P;%H;%s;%an;%cd;" --author="builder" --branches --all'
+    write_to_file_command = f" >> {com_com_log}"
+    call(cd_command + " && " + git_log_command + write_to_file_command, shell=True)
+
+
 if __name__ == "__main__":
     # values
     parent_dir = "/Users/natalia.murycheva/Documents/gitCommitMessageCollectorStorage"
     git_dir_name = "aurora"
+    git_dir = os.path.join(parent_dir, git_dir_name)
     com_com_log_file = f"gcm_{git_dir_name}_com_com_msg_author_date.log"
     com_com_log_file = os.path.join(parent_dir, com_com_log_file)
     tmp_file = os.path.join(parent_dir, "messages_statistics.txt")
+    builder_com_com_log = "builder_com_com_msg_author_date.log"
+    builder_com_com_log = os.path.join(parent_dir, builder_com_com_log)
+    builder_statistics_file = os.path.join(parent_dir, "builder_messages_statistics.txt")
