908b4e90e30a32e6a83b7e8060cf48b00124502b
natalia.murycheva <nataliamurycheva94@gmail.com>
2019-12-10 13:54:22 +0300

    add running for translate

--- NMT/natalymr/validate.sh
+++ NMT/natalymr/validate.sh
@@ -3,11 +3,17 @@
 dev=/home/ubuntu/crawler/data/nematus_data/test.diffs
 ref=/home/ubuntu/crawler/data/nematus_data/test.msg
 
-# run model on test set and get the score
+# run model on test set
+python ../../../nematus/nematus/translate.py \
+     -m ../../../nematus/models/model.first_try.npz  \
+     -i /home/ubuntu/crawler/data/nematus_data/test.diffs \
+     -o /home/ubuntu/crawler/data/nematus_data/test.msg.from_model \
+     -v
+
+# get the score
 python ../../../nematus/nematus/score.py \
      -m ../../../nematus/models/model.first_try.npz  \
-     -s /home/ubuntu/crawler/data/nematus_data/test.diffs \
+     -s /home/ubuntu/crawler/data/nematus_data/test.msg.from_model \
      -t /home/ubuntu/crawler/data/nematus_data/test.msg \
-     -o /home/ubuntu/crawler/data/nematus_data/test.msg.from_model \
      -v
 
